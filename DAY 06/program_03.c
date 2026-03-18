f15cf758171b193ad0bbec65bb9d660814630309
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-03-18 12:48:54 +0530

    Files

--- DAY 06/program_03.c
+++ DAY 06/program_03.c
@@ -6,8 +6,10 @@ int main() {
     if (fork() == 0) {
         printf("B (Child PID: %d, Parent PID: %d)\n", getpid(), getppid());
     }
-    if (fork() == 0) {
-        printf("C (Child PID: %d, Parent PID: %d)\n", getpid(), getppid());
+    else {
+        if (fork() == 0) {
+            printf("C (Child PID: %d, Parent PID: %d)\n", getpid(), getppid());
+        }
     }
     return 0;
 }
