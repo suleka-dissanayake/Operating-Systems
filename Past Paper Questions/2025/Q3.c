4f13021b1288f427bfd831ea850315986aa4912c
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-06-07 09:06:17 +0530

    Files

--- Past Paper Questions/2025/Q3.c
+++ Past Paper Questions/2025/Q3.c
@@ -1,73 +1,63 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main()
-{
+int main() {
     int p[2];
-    int n,i,heart,oxygen;
-    int critical=0;
+    int n, heart, oxygen;
+    int critical = 0;
+    int i;
 
     char name[50];
 
     pipe(p);
 
     printf("Enter number of patients: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    if(fork()==0)
-    {
+    if (fork() == 0) {
         close(p[1]);
 
         printf("\nPatient Report\n");
 
-        for(i=0;i<n;i++)
-        {
-            read(p[0],name,sizeof(name));
-            read(p[0],&heart,sizeof(int));
-            read(p[0],&oxygen,sizeof(int));
+        for (i = 0; i < n; i++) {
+            read(p[0], name, sizeof(name));
+            read(p[0], &heart, sizeof(int));
+            read(p[0], &oxygen, sizeof(int));
 
-            printf("%s ",name);
-            printf("%d ",heart);
-            printf("%d%% ",oxygen);
+            printf("%s ", name);
+            printf("%d ", heart);
+            printf("%d%% ", oxygen);
 
-            if(heart<60 || heart>100 || oxygen<90)
-            {
+            if (heart < 60 || heart > 100 || oxygen < 90) {
                 printf("CRITICAL\n");
                 critical++;
-            }
-            else if((heart>=60 && heart<=65) ||
-                    (heart>=95 && heart<=100) ||
-                    (oxygen>=90 && oxygen<=92))
-            {
+            } else if ((heart >= 60 && heart <= 65) ||
+                       (heart >= 95 && heart <= 100) ||
+                       (oxygen >= 90 && oxygen <= 92)) {
                 printf("ALERT\n");
-            }
-            else
-            {
+            } else {
                 printf("STABLE\n");
             }
         }
 
-        printf("Critical Cases = %d\n",critical);
-    }
-    else
-    {
+        printf("Critical Cases = %d\n", critical);
+    } else {
         close(p[0]);
 
-        for(i=0;i<n;i++)
-        {
+        for (i = 0; i < n; i++) {
             printf("\nName: ");
-            scanf("%s",name);
+            scanf("%s", name);
 
             printf("Heart Rate: ");
-            scanf("%d",&heart);
+            scanf("%d", &heart);
 
             printf("Oxygen Level: ");
-            scanf("%d",&oxygen);
+            scanf("%d", &oxygen);
 
-            write(p[1],name,sizeof(name));
-            write(p[1],&heart,sizeof(int));
-            write(p[1],&oxygen,sizeof(int));
+            write(p[1], name, sizeof(name));
+            write(p[1], &heart, sizeof(int));
+            write(p[1], &oxygen, sizeof(int));
         }
     }
 
