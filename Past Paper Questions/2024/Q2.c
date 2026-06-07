f619f8a01ca0cd5525416061b0b174ba3643b6a5
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-06-07 09:39:51 +0530

    Files

--- Past Paper Questions/2024/Q2.c
+++ Past Paper Questions/2024/Q2.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
     for (int i = 100; i <= 999; i++) {
         int n1 = i / 100;
         int n2 = (i / 10) % 10;
         int n3 = i % 10;
+
         if (i == n1*n1*n1 + n2*n2*n2 + n3*n3*n3) {
             printf("%d is an armstrong number!\n", i);
         }
     }
+    
     return 0;
 }
